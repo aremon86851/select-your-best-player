793b5acd150f051ec355ec95e6a8a0a8a2a79cbb
Abdur Rahman Emon <aremon86851@gmail.com>
2022-08-23 01:53:11 +0600

    fully completed website

--- js/common-function.js
+++ js/common-function.js
@@ -1,4 +1,10 @@
 function selectedPlayer(playerId, btnId) {
+    const countElement = document.querySelectorAll('li');
+    const countElementLength = countElement.length;
+    if (countElementLength === 5) {
+        alert('You are already selected five players');
+        return
+    }
     const playerNameContainer = document.getElementById(playerId);
     const listItemContainer = document.getElementById('list-item')
     const playerName = playerNameContainer.innerText;
@@ -11,6 +17,46 @@ function selectedPlayer(playerId, btnId) {
     selectedbtnId.style.backgroundColor = "grey";
     listItemContainer.appendChild(creatListitem);
 }
-function managerAndCoach() {
 
+function perPlayerPrices(perPlayerId, totalExpensesId) {
+    const perPlayerPriceContainer = document.getElementById(perPlayerId);
+    const perPlayerPriceString = perPlayerPriceContainer.value;
+    const perPlayerPrice = parseFloat(perPlayerPriceString);
+    if (isNaN(perPlayerPrice)) {
+        alert('Please! Enter a number in "PER PLAYER" field')
+        return
+    }
+    const countElement = document.querySelectorAll('li');
+    const countElementLength = countElement.length;
+    const totalExpenses = perPlayerPrice * countElementLength;
+    const totalExpensesContainer = document.getElementById(totalExpensesId);
+
+    totalExpensesContainer.innerText = totalExpenses;
+}
+
+function perPlayerTotal(totalId) {
+    const totalExpensesContainers = document.getElementById(totalId);
+    const totalExpensesNumber = totalExpensesContainers.innerText;
+    const totalExpensesAmount = parseFloat(totalExpensesNumber);
+    return totalExpensesAmount;
+}
+function managerSalaryAmount(managerId) {
+    const managerSalaryContainer = document.getElementById(managerId);
+    const managerSalaryString = managerSalaryContainer.value;
+    const managerSalary = parseFloat(managerSalaryString);
+    if (isNaN(managerSalary)) {
+        alert('Please! Enter a number in "MANAGER" field')
+        return
+    }
+    return managerSalary;
+}
+function coachSalaryId(coachId) {
+    const coachSalaryContainer = document.getElementById(coachId);
+    const coachSalaryString = coachSalaryContainer.value;
+    const coachSalary = parseFloat(coachSalaryString);
+    if (isNaN(coachSalary)) {
+        alert('Please! Enter a number in "COACH" field')
+        return
+    }
+    return coachSalary;
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.getElementById('lionel-messi').addEventListener('click', function () {
     selectedPlayer('lionel-messi-heading', 'lionel-messi');
-})
 
+})
 document.getElementById('jr-neymar').addEventListener('click', function () {
     selectedPlayer('jr-neymar-heading', 'jr-neymar');
 })
@@ -25,33 +25,15 @@ document.getElementById('edinson-cavani').addEventListener('click', function () 
 })
 document.getElementById('renato-sanches').addEventListener('click', function () {
     selectedPlayer('renato-sanches-heading', 'renato-sanches');
-
 })
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const perPlayerPriceContainer = document.getElementById('per-player-price-id');
-    const perPlayerPriceString = perPlayerPriceContainer.value;
-    const perPlayerPrice = parseFloat(perPlayerPriceString);
-    const countElement = document.querySelectorAll('li');
-    const countElementLength = countElement.length;
-    if (countElementLength == '5') {
-        alert('You are selected alreay 5')
-    }
-    const totalExpenses = perPlayerPrice * countElementLength;
-    const totalExpensesContainer = document.getElementById('total-expenses');
-    totalExpensesContainer.innerText = totalExpenses;
+    perPlayerPrices('per-player-price-id', 'total-expenses')
 })
 document.getElementById('calculate-total-btn').addEventListener('click', function () {
-    const totalExpensesContainers = document.getElementById('total-expenses');
-    const totalExpensesNumber = totalExpensesContainers.innerText;
-    const totalExpensesAmount = parseFloat(totalExpensesNumber);
-
-    const managerSalaryContainer = document.getElementById('manager-salary-id');
-    const managerSalaryString = managerSalaryContainer.value;
-    const managerSalary = parseFloat(managerSalaryString);
-
-    const coachSalaryContainer = document.getElementById('coach-salary-id');
-    const coachSalaryString = coachSalaryContainer.value;
-    const coachSalary = parseFloat(coachSalaryString);
+    const totalExpensesAmount = perPlayerTotal('total-expenses');
+    const managerSalary = managerSalaryAmount('manager-salary-id');
+    const coachSalary = coachSalaryId('coach-salary-id');
 
     const inTotalExpensesContainer = document.getElementById('total-expenses-amount');
     const inTotalExpenses = totalExpensesAmount + managerSalary + coachSalary;
